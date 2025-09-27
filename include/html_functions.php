b0c6e8a1549f3285f8dd0161a2e59b8d6e364701
Hristo Sabev <thefoxybg@gmail.com>
2025-09-27 11:00:29 +0300

    task/5 - [WIP] Added basic frontend

--- include/html_functions.php
+++ include/html_functions.php
@@ -1,2 +1,294 @@
 <?php
 
+function headerContainer(): void
+{
+
+?>
+
+
+    <!DOCTYPE html>
+    <html dir="ltr" lang="en-US">
+
+    <head>
+
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="author" content="SemiColonWeb" />
+
+        <link href="https://fonts.googleapis.com/css?family=Dosis:400,500,600,700|Open+Sans:400,600,700|Dancing+Script&display=swap" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="./css/bootstrap.css" type="text/css" />
+        <link rel="stylesheet" href="./css/style.css" type="text/css" />
+        <link rel="stylesheet" href="./css/dark.css" type="text/css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="./css/font-icons.css" type="text/css" />
+        <link rel="stylesheet" href="./css/animate.css" type="text/css" />
+        <link rel="stylesheet" href="./css/magnific-popup.css" type="text/css" />
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link rel="stylesheet" href="css/burger.css" type="text/css" />
+        <link rel="stylesheet" href="css/fonts.css" type="text/css" />
+
+        <script src="js/js-cart-palermo.js"></script>
+
+
+
+    <?php }
+
+
+function footerContainer(): void
+{
+
+    ?>
+
+
+        <footer id="footer" class="dark noborder parallax pt-3" style="background-image: url('images/others/section-5.jpg');" data-top-bottom="background-position:100% -300px" data-bottom-top="background-position: 100% 300px">
+            <div class="container">
+
+                <div class="footer-widgets-wrap">
+
+                    <div class="row col-mb-50">
+                        <div class="col-lg-8">
+
+
+                            <div class="row col-mb-30">
+                                <div class="col-6 col-lg-4">
+                                    <div class="widget">
+
+                                        <h4 class="fw-bold">ЗАПОВЯДАЙТЕ ПРИ НАС</h4>
+
+                                        <ul class="list-unstyled ms-0">
+                                            <li class="mb-2"><a class="text-white-50" href="#">+359 78 98 88 71</a></li>
+                                            <li class="mb-2"><a class="text-white-50" href="#">+359 885 83 51 71</a></li>
+                                            <li class="mb-2"><a class="text-white-50" href="#">office@pizzaroyalkn.eu</a></li>
+                                            <li class="mb-2"><a class="text-white-50" href="#">facebook.com/royal.kn</a></li>
+                                        </ul>
+                                        ул. Власина 2
+                                        Кюстендил, България
+
+                                    </div>
+                                </div>
+                                <div class="col-6 col-lg-4">
+                                    <div class="widget">
+
+                                        <h4 class="fw-bold">Полезно</h4>
+
+                                        <ul class="list-unstyled ms-0">
+                                            <li class="mb-2"><a class="text-white-50" href="#">Начало</a></li>
+                                            <li class="mb-2"><a class="text-white-50" href="#">Меню</a></li>
+                                            <li class="mb-2"><a class="text-white-50" href="#">Контакти</a></li>
+                                            <li class="mb-2"><a class="text-white-50" href="#">Помощ</a></li>
+                                        </ul>
+
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+
+                    </div>
+
+                </div><!-- .footer-widgets-wrap end -->
+            </div>
+
+            <!-- Copyrights
+			============================================= -->
+            <div id="copyrights">
+                <div class="container">
+
+                    <div class="row col-mb-30">
+                        <div class="col-md-6 text-center text-md-start">
+                            &copy; Пицария Роял, Кюстендил. Всички права запазени.<br>
+                            <div class="copyright-links"><a href="https://balikgstudio.eu">Поддържа се от BalikG Studio.</a></div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div><!-- #copyrights end -->
+        </footer><!-- #footer end -->
+
+        </div><!-- #wrapper end -->
+
+        <!-- Go To Top
+	============================================= -->
+        <div id="gotoTop" class="fas fa-arrow-up"></div>
+
+        <!-- JavaScripts
+	============================================= -->
+        <script src="js/jquery.js"></script>
+        <script src="js/plugins.min.js"></script>
+        <script src="js/hover-animate.js"></script>
+
+        <!-- Footer Scripts
+	============================================= -->
+        <script src="js/functions.js"></script>
+
+    <?php }
+
+
+function navbarContainer(): void
+{
+
+    ?>
+
+        <!-- Header
+		============================================= -->
+        <header id="header" class="transparent-header dark" data-sticky-class="dark-color" data-sticky-shrink-offset="0">
+            <div id="header-wrap">
+                <div class="container">
+                    <div class="header-row">
+
+                        <!-- Logo
+						============================================= -->
+                        <div id="logo">
+                            <a href="index.php" class="standard-logo" data-dark-logo="images/royal_logo.png" data-sticky-logo="images/royal_logo.png"><img src="images/royal_logo.png" alt="Royal Logo"></a>
+                            <a href="index.php" class="retina-logo" data-dark-logo="images/royal_logo.png" data-sticky-logo="images/royal_logo.png"><img src="images/royal_logo.png" alt="Royal Logo"></a>
+                        </div><!-- #logo end -->
+
+
+                        <div id="primary-menu-trigger">
+                            <svg class="svg-trigger" viewBox="0 0 100 100">
+                                <path d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"></path>
+                                <path d="m 30,50 h 40"></path>
+                                <path d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"></path>
+                            </svg>
+                        </div>
+
+                        <!-- Primary Navigation
+						============================================= -->
+                        <nav class="primary-menu">
+
+                            <ul class="one-page-menu menu-container" data-easing="easeInOutExpo" data-speed="1250" data-offset="60">
+                                <li class="menu-item"><a class="menu-link" href="https://dev.balikgstudio.eu/pizzaroyalkn/">
+                                        <div>Начало</div>
+                                    </a></li>
+                                <li class="menu-item"><a class="menu-link" href="#">
+                                        <div>Меню</div>
+                                    </a>
+                                    <ul class="sub-menu-container rounded-bottom">
+                                        <li class="menu-item"><a class="menu-link" href="salati">
+                                                <div>Салати</div>
+                                            </a></li>
+                                        <li class="menu-item"><a class="menu-link" href="supi">
+                                                <div>Супи</div>
+                                            </a></li>
+                                        <li class="menu-item"><a class="menu-link" href="predqstiq">
+                                                <div>Топли предястия</div>
+                                            </a></li>
+                                        <li class="menu-item"><a class="menu-link" href="pileshko">
+                                                <div>Ястия с пилешко</div>
+                                            </a></li>
+                                        <li class="menu-item"><a class="menu-link" href="svinsko">
+                                                <div>Ястия със свинско</div>
+                                            </a></li>
+                                        <li class="menu-item"><a class="menu-link" href="riba">
+                                                <div>Риба</div>
+                                            </a></li>
+                                        <li class="menu-item"><a class="menu-link" href="zapekanki">
+                                                <div>Запеканки</div>
+                                            </a></li>
+                                        <li class="menu-item"><a class="menu-link" href="spageti">
+                                                <div>Спагети</div>
+                                            </a></li>
+                                        <li class="menu-item"><a class="menu-link" href="pici">
+                                                <div>Пици и бургери</div>
+                                            </a></li>
+                                        <li class="menu-item"><a class="menu-link" href="hlebcheta">
+                                                <div>Хлебчета</div>
+                                            </a></li>
+                                        <li class="menu-item"><a class="menu-link" href="sosove">
+                                                <div>Сосове</div>
+                                            </a></li>
+                                        <li class="menu-item"><a class="menu-link" href="deserti">
+                                                <div>Десерти</div>
+                                            </a></li>
+                                    </ul>
+                                </li>
+                                <li class="menu-item"><a class="menu-link" href="#">
+                                        <div>Контакти</div>
+                                    </a></li>
+                            </ul>
+
+                        </nav><!-- #primary-menu end -->
+
+                        <!-- Cart
+                        ============================================= -->
+                        <div class="js-cart-dropdown d-flex align-items-center ms-auto">
+                            <a href="#" class="js-cart-dropdown__trigger d-flex align-items-center text-white">
+                                <i class="fas fa-shopping-cart me-2"></i>
+                                <span class="js-cart-dropdown__counter bg-danger text-white rounded-circle d-inline-flex align-items-center justify-content-center">1</span>
+                            </a>
+                            <div class="js-cart-dropdown__content shadow-lg rounded">
+                                <div class="js-cart-dropdown__header p-3 border-bottom">
+                                    <h4 class="mb-0">Shopping Cart</h4>
+                                </div>
+                                <div class="js-cart-dropdown__items p-3">
+                                    <!-- Dummy cart item -->
+                                    <div class="js-cart-dropdown__item d-flex align-items-center pb-3 border-bottom">
+                                        <div class="js-cart-dropdown__item-image me-3">
+                                            <img src="images/menu-items/пилешка_супа.jpg" alt="Пилешка супа" class="js-cart-dropdown__item-img">
+                                        </div>
+                                        <div class="js-cart-dropdown__item-desc flex-grow-1">
+                                            <div class="js-cart-dropdown__item-title fw-bold">Пилешка супа</div>
+                                            <div class="js-cart-dropdown__item-price text-muted">1x 4.50 лв</div>
+                                        </div>
+                                        <div class="js-cart-dropdown__item-actions">
+                                            <a href="#" class="js-cart-dropdown__remove text-danger"><i class="fas fa-times"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="js-cart-dropdown__footer p-3 border-top">
+                                    <div class="js-cart-dropdown__total d-flex justify-content-between mb-3">
+                                        <span class="fw-bold">Total:</span>
+                                        <span class="fw-bold text-danger">4.50 лв</span>
+                                    </div>
+                                    <div class="d-grid gap-2">
+                                        <a href="#" class="btn btn-sm js-cart-dropdown__view-btn">View Cart</a>
+                                        <a href="#" class="btn btn-danger btn-sm js-cart-dropdown__order-btn">Checkout</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="header-wrap-clone"></div>
+        </header><!-- #header end -->
+
+    <?php }
+
+
+function sliderContainer(): void
+{
+
+    ?>
+
+
+        <!-- Slider
+		============================================= -->
+        <section id="slider" class="slider-element min-vh-100 page-section slide-img include-header" data-animate="img-to-right" style="background: url('images/hero_bg.png') center center no-repeat; background-size: cover;">
+            <div class="slider-inner">
+
+                <div class="vertical-middle">
+                    <div class="container dark">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-8 dotted-bg parallax" data-start="top: 0px; opacity: 1" data-400="top: 50px; opacity: 0.3">
+                                <div class="emphasis-title" data-animate="fadeInUp">
+                                    <div class="before-heading font-secondary color">Pizza & Grill</div>
+                                    <h1 class="font-border"> Palermo!</h1>
+                                </div>
+                                <p class="lead text-white-50" data-animate="fadeInUp" data-delay="100">We're an Italian restaurant, located in the heart of the city, serving authentic Italian cuisine.</p>
+                                <a data-scrollto="#delivery" data-easing="easeInOutExpo" data-speed="1250" class="button button-large button-rounded px-4 button-border button-light button-white fw-semibold" data-animate="fadeInUp" data-delay="200">Delivery</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+    <?php }
--- index.php
+++ index.php
@@ -1,27 +1,238 @@
 <?php
 require_once(__DIR__ . '/include/connect.php');
-
-$stmt = $pdo->prepare("SELECT * FROM admins");
-$stmt->execute();
-$admins = $stmt->fetchAll();
-
-foreach ($admins as $admin) {
-    echo "Admin: " . ($admin['admin_name']) . "<br>";
-}
-
+include(__DIR__ . '/include/html_functions.php');
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<?php headerContainer(); ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="favicon.ico">
-    <title><?php echo SITE_TITLE; ?></title>
+<title>Palermo</title>
 </head>
 
-<body>
+<body class="stretched">
+
+	<div class="body-overlay"></div>
+
+	<div id="side-panel" class="dark" style="background: #101010 url('images/icon-bg-white.png') repeat center center;"></div>
+
+
+	<div id="wrapper" class="clearfix">
+		<div class="col-12 col-md-auto">
+
+			<div class="top-links text-center">
+				<ul class="top-links-container">
+					<li class="top-links-item"><a href="#"><i class="fas fa-truck"></i>Order now from 9:00 AM to 10:00 PM</a></li>
+					<li class="top-links-item"><a href="#"><i class="fas fa-phone"></i> 088 480 0809 or 078 98 88 71</a></li>
+					<li class="top-links-item"><a href="#"><i class="fas fa-envelope"></i>support@palermo.com</a></li>
+				</ul>
+			</div>
+
+		</div>
+
+		<?php navbarContainer(); ?>
+
+		<?php sliderContainer(); ?>
+
+
+		<!-- MAIN CONTENT -->
+
+		<section id="content" class="dark-color">
+			<div class="content-wrap p-0">
+
+				<div id="story" class="page-section">
+
+
+					<div class="section m-0 p-0">
+					</div>
+
+					<div class="section dark-color m-0 p-0">
+						<div class="container dark">
+							<div class="clear"></div>
+						</div>
+					</div>
+				</div>
+
+				<div class="clear"></div>
+
+				<!-- Today's Menu Section -->
+				<div id="todays-menu" class="section dark m-0" style="padding: 80px 0; background: #101010 url('images/icon-bg-white.png') repeat center center;">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-5 dark order-2 order-md-1">
+								<div class="bottommargin">
+									<div class="d-flex align-items-center dotted-bg">
+										<h1 class="font-border display-4 ls1 fw-bold mb-0 ms-3">Today's Menu</h1>
+									</div>
+								</div>
+
+								<div class="clear"></div>
+
+								<!-- Hardcoded menu items -->
+								<div class="price-menu-warp img-hover-block" data-img="images/menu-items/пилешка_супа.jpg">
+									<div class="price-header">
+										<div class="price-name color">Chicken Soup</div>
+										<div class="price-dots">
+											<span class="separator-dots"></span>
+										</div>
+										<div class="price-price">300g - 4.50 lv</div>
+									</div>
+									<p class="price-desc">Traditional chicken soup with homemade noodles and fresh vegetables.</p>
+								</div>
+
+								<div class="price-menu-warp img-hover-block" data-img="images/menu-items/Пилешки-филенца-с-корнфлейкс.jpg">
+									<div class="price-header">
+										<div class="price-name color">Cornflake Chicken Fillets</div>
+										<div class="price-dots">
+											<span class="separator-dots"></span>
+										</div>
+										<div class="price-price">250g - 12.90 lv</div>
+									</div>
+									<p class="price-desc">Crispy chicken fillets breaded in cornflakes, served with fries.</p>
+								</div>
+
+								<div class="price-menu-warp img-hover-block" data-img="images/menu-items/burgers/double.jpg">
+									<div class="price-header">
+										<div class="price-name color">Double Burger</div>
+										<div class="price-dots">
+											<span class="separator-dots"></span>
+										</div>
+										<div class="price-price">400g - 15.90 lv</div>
+									</div>
+									<p class="price-desc">Juicy double burger with beef, cheese, lettuce, and special sauce.</p>
+								</div>
+
+							</div>
+							<div class="col-md-7 order-1 order-md-2">
+								<div class="slide-img" data-animate="img-to-right">
+									<img src="images/svg/items.svg" alt="Today's Menu" style="max-width: 100%; height: auto;">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+			</div>
+
+			<div class="section dark my-0" style="padding: 60px 0 120px; background: linear-gradient(to bottom, #101010, transparent), url('images/icon-bg-white.png') repeat center center;">
+				<div class="container mx-auto" style="max-width: 1000px">
+					<div class="row align-items-center">
+						<div class="col-md-6 text-center text-md-start">
+							<h2 class="display-4 fw-bold text-uppercase ls1" data-animate="fadeInUp">За доставка!</h2>
+							<h4 class="font-secondary text-white-50" data-animate="fadeInUp" data-delay="100"> БЕЗПЛАТНА ДОСТАВКА при поръчка на стойност над 10.00 лв. в рамките на гр. Кюстендил!</h4>
+						</div>
+						<div class="col-md-6">
+							<div class="slide-img" data-animate="img-to-left">
+								<img src="images/svg/delivery.svg" alt="">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- Blog Section -->
+			<div id="blog" class="section dark m-0" style="padding: 80px 0; background: #1a1a1a;">
+				<div class="container">
+					<div class="row mb-5">
+						<div class="col-12 text-center">
+							<div class="d-flex align-items-center justify-content-center dotted-bg mb-4">
+								<h1 class="font-border display-4 ls1 fw-bold mb-0">Latest News</h1>
+							</div>
+							<p class="lead text-white-50">Discover the latest news, recipes, and stories from our kitchen</p>
+						</div>
+					</div>
+					<div class="row">
+						<!-- Blog Article 1 -->
+						<div class="col-lg-4 col-md-6 mb-4">
+							<div class="card bg-dark border-secondary h-100" style="transition: transform 0.3s ease; border: 1px solid #444;">
+								<img src="images/menu-items/пилешка_супа.jpg" class="card-img-top" alt="Traditional Recipes" style="height: 200px; object-fit: cover;">
+								<div class="card-body d-flex flex-column">
+									<div class="mb-2">
+										<span class="badge bg-danger mb-2">Recipes</span>
+										<small class="text-muted d-block">September 27, 2025</small>
+									</div>
+									<h5 class="card-title text-white">Secrets of Traditional Chicken Soup</h5>
+									<p class="card-text text-white-50 flex-grow-1">We reveal the secrets of our most popular soup - how to prepare the perfect chicken soup with homemade noodles and fresh vegetables.</p>
+									<a href="#" class="btn btn-outline-light btn-sm mt-auto">Read More</a>
+								</div>
+							</div>
+						</div>
+
+						<!-- Blog Article 2 -->
+						<div class="col-lg-4 col-md-6 mb-4">
+							<div class="card bg-dark border-secondary h-100" style="transition: transform 0.3s ease; border: 1px solid #444;">
+								<img src="images/menu-items/burgers/double.jpg" class="card-img-top" alt="New Menu Items" style="height: 200px; object-fit: cover;">
+								<div class="card-body d-flex flex-column">
+									<div class="mb-2">
+										<span class="badge bg-success mb-2">News</span>
+										<small class="text-muted d-block">September 25, 2025</small>
+									</div>
+									<h5 class="card-title text-white">New Burgers on the Menu</h5>
+									<p class="card-text text-white-50 flex-grow-1">Introducing our new creations - a double burger with a unique combination of flavors. Try it today with free delivery.</p>
+									<a href="#" class="btn btn-outline-light btn-sm mt-auto">Read More</a>
+								</div>
+							</div>
+						</div>
+
+						<!-- Blog Article 3 -->
+						<div class="col-lg-4 col-md-6 mb-4">
+							<div class="card bg-dark border-secondary h-100" style="transition: transform 0.3s ease; border: 1px solid #444;">
+								<img src="images/svg/delivery.svg" class="card-img-top bg-secondary p-4" alt="Delivery Service" style="height: 200px; object-fit: contain;">
+								<div class="card-body d-flex flex-column">
+									<div class="mb-2">
+										<span class="badge bg-info mb-2">Services</span>
+										<small class="text-muted d-block">September 20, 2025</small>
+									</div>
+									<h5 class="card-title text-white">Improved Delivery in Kyustendil</h5>
+									<p class="card-text text-white-50 flex-grow-1">We have expanded our delivery area and reduced delivery times. Free delivery for orders over 10 lv within the city.</p>
+									<a href="#" class="btn btn-outline-light btn-sm mt-auto">Read More</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					
+					<div class="row mt-4">
+						<div class="col-12 text-center">
+							<a href="#" class="btn btn-danger btn-lg">View All Articles</a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div id="delivery" class="section page-section dark m-0 pb-0 pb-md-5 slide-img" data-animate="img-to-left" style="background: #101010 url('images/icon-bg-white.png') repeat center center;">
+				<div class="container pt-3 pb-4">
+					<div class="row">
+						<div class="col-sm-5" style="line-height: 1.7; z-index: 1">
+							<address class="d-block mb-5">
+								<div class="font-secondary h5 mb-2 color">Address:</div>
+								<span class="h6 text-white ls1 fw-normal font-primary">
+									Dobrich, st. Kaliakra 55<br>
+									Кюстендил, България<br>
+								</span>
+							</address>
+							<div class="font-secondary h5 mb-2 color">Call for Delivery:</div>
+							<p class="d-block h6 text-white ls1 fw-normal font-primary mb-5">+359 78 98 88 71</p>
+							<p class="d-block h6 text-white ls1 fw-normal font-primary mb-5">+359 885 83 51 71</p>
+
+							<div class="font-secondary h5 mb-2 color">Email:</div>
+							<a href="mailto:support@palermo.com" class="d-block h6 text-white ls1 fw-normal font-primary mb-5">support@palermo.com</a>
+
+							<div class="font-secondary h5 mb-2 color">Working Hours:</div>
+							<div class="h6 text-white ls1 fw-normal font-primary">Every day from <b>10:00 AM</b> to <b>12:00 AM</b><br>
+								Delivery is available every day from <b>11:00 AM</b> to <b>11:00 PM</b></div>
+						</div>
+						<span class="text-uppercase text-white ls1 fw-normal font-primary"><b>FREE DELIVERY</b> for orders over <b>25.00 lv.</b></span>
+					</div>
+				</div>
+			</div>
+
+	</div>
+
+	</div>
+
+	</section>
+
+
+	<?php footerContainer(); ?>
 
 </body>
 
