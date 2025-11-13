2f6c6cf3fd01f52c1976bad3b0452ccc07ee9eb0
Hristo Sabev <82899133+Foxyball@users.noreply.github.com>
2025-11-13 09:18:26 +0200

    Remove error logging
    Remove error logging for blog creation failure.

--- admin/blog_add.php
+++ admin/blog_add.php
@@ -75,7 +75,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             header('Location: blog_list');
             exit;
         } catch (PDOException $e) {
-            error_log('Blog creation error: ' . $e->getMessage());
             $errors[] = 'There was an error creating the blog post. Please try again.';
         }
     }
