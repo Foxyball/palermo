1a661dfb239e0e1f3af26fe63ec5e52834f01a67
Hristo Sabev <thefoxybg@gmail.com>
2025-12-06 00:01:12 +0200

    Snacks and Beverages 404 #196

--- sections/featured-categories.php
+++ sections/featured-categories.php
@@ -13,18 +13,18 @@
 					<div class="item-categories overflow-hidden col-md-4 dark col-padding position-relative" style="background-color: #1B1B1B;">
 						<div class="cat-img"><img  src="images/3.png" alt="Category Image"></div>
 						<div class="cat-text">
-							<h2 class="fw-bold ls-1 text-uppercase mb-5">Snacks</h2>
+							<h2 class="fw-bold ls-1 text-uppercase mb-5">Pizza</h2>
 							<p class="mb-5">Savor our delicious snacks, perfect for any time of day. From crispy fries to flavorful appetizers, we have something for everyone.</p>
-							<a href="<?php echo BASE_URL; ?>cat/snacks" data-scrollto="#menu-snacks" data-easing="easeInOutExpo" data-speed="1250" class="button button-rounded px-4 button-border button-light button-white fw-semibold ms-0">View All</a>
+							<a href="<?php echo BASE_URL; ?>cat/pizza" data-scrollto="#menu-snacks" data-easing="easeInOutExpo" data-speed="1250" class="button button-rounded px-4 button-border button-light button-white fw-semibold ms-0">View All</a>
 						</div>
 					</div>
 
 					<div class="item-categories overflow-hidden col-md-4 dark col-padding position-relative" style="background-color: #262626;">
 						<div class="cat-img"><img  src="images/4.png" alt="Category Image"></div>
 						<div class="cat-text">
-							<h2 class="fw-bold ls-1 text-uppercase mb-5">Beverages</h2>
+							<h2 class="fw-bold ls-1 text-uppercase mb-5">Pasta</h2>
 							<p class="mb-5">Quench your thirst with our refreshing beverages. From classic sodas to specialty drinks, we have the perfect drink to complement your meal.</p>
-							<a href="<?php echo BASE_URL; ?>cat/beverages" data-scrollto="#menu-beverages" data-easing="easeInOutExpo" data-speed="1250" class="button button-rounded px-4 button-border button-light button-white fw-semibold ms-0">View All</a>
+							<a href="<?php echo BASE_URL; ?>cat/pasta" data-scrollto="#menu-beverages" data-easing="easeInOutExpo" data-speed="1250" class="button button-rounded px-4 button-border button-light button-white fw-semibold ms-0">View All</a>
 						</div>
 					</div>
 
